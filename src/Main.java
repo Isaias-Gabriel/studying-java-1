8145ce17a75d141664d61d2394025731718d2c86
Isaias-Gabriel <hyzegabriel@gmail.com>
2023-01-30 09:54:50 -0300

    'initial commit'

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int myInt = 7;
+        float myFloat = 3.23f;
+        double myDouble = 7.73;
+        String myString = "Hello!";
+
+        double anotherDouble = myInt;
+        int anotherInt = (int) myDouble;
+
+        int someInt;
+        someInt = 9;
+
+        int[] myIntArray = {1, 2, 3, 4, 7};
+
+        System.out.println(myString.toUpperCase());
+        System.out.println(myIntArray[2]);
+
+    }
+}
